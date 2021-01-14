ad8e930ce24ac93bcc66ff7b1a62f83dfbce8aa7
trufan0ff <trufan.off@yandex.ru>
2021-01-14 16:42:44 +0300

    dodelal

--- scripts/index.js
+++ scripts/index.js
@@ -13,24 +13,41 @@ const popupName = document.querySelector('.popup__name')
 const popupActivity = document.querySelector('.popup__activity')
 
 
-const togglePopup = () => {
-    overlay.classList.toggle('overlay_active')
-    popupActivity.value = subtitleName.textContent
-    popupName.value = titleName.textContent
-    popupName.textContent = titleName.value
-    popupActivity.textContent = subtitleName.value
+
+const togglePopup = () => {                         
+    overlay.classList.toggle('overlay_active')      /* добавляет класс overlay_active либо удаляет его(меняет значение на противоположный) */
 }
-openButton.addEventListener('click', togglePopup)
 
-closeButton.addEventListener('click', togglePopup)
+const innerName = () => {
+    popupActivity.value = subtitleName.textContent  /* берет из профиля значение субтитл и записывает его в форму хобби */
+    popupName.value = titleName.textContent         /* берет из профиля значение титл и записывает его в форму Имя */
+}
 
-overlay.addEventListener('click', (event) => {
-    if (event.target === event.currentTarget) {
+const innerInfo = () => {
+    const profileEditInfo = document.querySelector('.profile__info')
+    profileEditInfo.innerHTML = `<div class="profile__edit">
+    <h2 class="profile__title">${popupName.value}</h2>
+    <button class="profile__edit-button" type="button"></button>
+    </div>
+    <p class="profile__subtitle">${popupActivity.value}</p>`
+    titleName.textContent = popupName.value
+    subtitleName.textContent = popupActivity.value
+}
+
+
+openButton.addEventListener('click', togglePopup)   /* При нажатии кнопки редактирования вызывает функцию togglePopup() */ /* колбэк */
+openButton.addEventListener('click', innerName)
+
+closeButton.addEventListener('click', togglePopup)  /* При нажатии кнопки закрытия(крестик) вызывает функцию togglePopup() */ /* колбэк */
+
+overlay.addEventListener('click', (event) => {      /* Клик по оверлею закрывает форму */
+    if (event.target === event.currentTarget) {     /* определяет куда нажал пользователь(непонятная магия) */
         togglePopup()
     }
 })
 
-const form = overlay.querySelector('.popup__form')
-form.addEventListener('submit', event => {
-    event.preventDefault(togglePopup())
+const form = overlay.querySelector('.popup__form')  /* переменная формы */
+form.addEventListener('submit', event => {          /* event - функция обработчик стандартного события */
+    event.preventDefault(togglePopup())             /* отмена стандартного события(не перезагрузит страницу) */
+    event.preventDefault(innerInfo())
 })
