80c480b7def1ce5da83b25ef064500ff52641bd1
trufan0ff <trufan.off@yandex.ru>
2021-01-17 19:39:06 +0300

    добавил миксы,реорганизовал структуру файлов

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 const openButton = document.querySelector('.profile__edit-button')
 
-const overlay = document.querySelector('.overlay')
+const popup = document.querySelector('.popup')
 
-const closeButton = overlay.querySelector('.popup__close')
+const closeButton = popup.querySelector('.popup__close')
 
 const titleName = document.querySelector('.profile__title')
 
 const subtitleName = document.querySelector('.profile__subtitle')
 
-const popupName = document.querySelector('.popup__name')
+const popupName = document.querySelector('.popup__name_firstName')
 
-const popupActivity = document.querySelector('.popup__activity')
+const popupActivity = document.querySelector('.popup__name_activity')
 
 
 
 const togglePopup = () => {                         
-    overlay.classList.toggle('overlay_active')      /* добавляет класс overlay_active либо удаляет его(меняет значение на противоположный) */
+    popup.classList.toggle('popup_active')      /* добавляет класс overlay_active либо удаляет его(меняет значение на противоположный) */
 }
 
 const innerName = () => {
@@ -34,13 +34,13 @@ openButton.addEventListener('click', innerName)
 
 closeButton.addEventListener('click', togglePopup)  /* При нажатии кнопки закрытия(крестик) вызывает функцию togglePopup() */ /* колбэк */
 
-overlay.addEventListener('click', (event) => {      /* Клик по оверлею закрывает форму */
+popup.addEventListener('click', (event) => {      /* Клик по оверлею закрывает форму */
     if (event.target === event.currentTarget) {     /* определяет куда нажал пользователь(непонятная магия) */
         togglePopup()
     }
 })
 
-const form = overlay.querySelector('.popup__form')  /* переменная формы */
+const form = popup.querySelector('.popup__form')  /* переменная формы */
 form.addEventListener('submit', event => {          /* event - функция обработчик стандартного события */
     event.preventDefault()  
     togglePopup()                                   /* отмена стандартного события(не перезагрузит страницу) */
